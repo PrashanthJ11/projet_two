f9d0bee843e294813ee2fdc577a7ba82d122ab71
PrashanthJ11 <jangamprashanthjaya@gmail.com>
2023-11-08 11:30:33 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def Prashanth(request):
+    return HttpResponse('<h2>This is Prashanth,i am searching for a job</h2>')
+# Create your views here.
